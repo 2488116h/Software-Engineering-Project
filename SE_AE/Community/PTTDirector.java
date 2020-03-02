7f2fa989f4bcaf41efdf2f72261d53c0ccf12b1a
He Wan <2470967w@student.gla.ac.uk>
2020-03-02 16:33:05 +0000

    Test3

--- SE_AE/Community/PTTDirector.java
+++ SE_AE/Community/PTTDirector.java
@@ -5,15 +5,15 @@ import java.io.PrintStream;
 import User.Account;
 
 public class PTTDirector extends Account{
-	
-	
+
+
 	public PTTDirector(String u, String p) {
 		super(u, p);
 	}
-	 
-	// approve class request or training request 
+
+	// approve class request or training request
 	public void approve(Object obj) {
-		if(obj instanceof ClassRequest) {// approve class request by set the status to 'Approved'		
+		if(obj instanceof ClassRequest) {// approve class request by set the status to 'Approved'
 			((ClassRequest) obj).setReqStatus(ClassRequest.status[1]);
 		}
 		if(obj instanceof ListOfTeacher) {// approve training request by add training information
@@ -21,19 +21,17 @@ public class PTTDirector extends Account{
 			for(Teacher teacher:trainingReq.getTeachers()) {
 				teacher.setStatus("attended 2020 term training");
 			}
-		}			
-		
+		}
+
 	}
-	
-	// disapprove class request or training request 
+
+	// disapprove class request or training request
 	public void diapprove(Object obj) {
-		if(obj instanceof ClassRequest) {// disapprove class request by set the status to 'Disapproved'		
+		if(obj instanceof ClassRequest) {// disapprove class request by set the status to 'Disapproved'
 			((ClassRequest) obj).setReqStatus(ClassRequest.status[2]);
 		}
 		// disapprove training request by clearing "pending" teachers in the training request list
 		// teachers who have been approved before will not be affected
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 		if (obj instanceof ListOfTeacher) {
 			ListOfTeacher trainingReq = (ListOfTeacher) obj;
@@ -41,24 +39,19 @@ public class PTTDirector extends Account{
 				if (t.getStatus().equals("Pending"))
 					trainingReq.removeTeacher(t);
 			}
-=======
-=======
->>>>>>> parent of 790501d... final updated
+
 		if(obj instanceof ListOfTeacher) {
 			ListOfTeacher trainingReq=(ListOfTeacher)obj;
 			for(Teacher t:trainingReq.getTeachers()) {
 				if(t.getStatus().equals("pending"))
 				trainingReq.removeTeacher(t);
-<<<<<<< HEAD
->>>>>>> parent of 790501d... final updated
-=======
->>>>>>> parent of 790501d... final updated
+
 		}
 			}
-			
+
 	}
-	
-	
+
+
 	public void print(PrintStream ps) {
 		super.print(ps);
 		ps.print(" PPT Director");
